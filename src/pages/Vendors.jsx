af40670d891d4c724c9651b58ef88aa04affff60
Gary Firzon <gfirzon@yahoo.com>
2025-11-06 21:39:37 -0600

    installed bs icons

--- src/pages/Vendors.jsx
+++ src/pages/Vendors.jsx
@@ -3,6 +3,12 @@ export default function Vendors() {
     <div className="container mt-5">
       <h1>Vendors</h1>
       <p>List of vendors will appear here.</p>
+      
+      <div className="d-flex justify-content-end mb-2">
+        <button id="refreshBtn" className="btn btn-outline-secondary" title="Refresh">
+          <i className="bi bi-arrow-clockwise"></i>
+        </button>
+      </div>
     </div>
   );
 }
