a5b4dd4884e2a8c2791180ffff4421b5110c85a5
Gary Firzon <gfirzon@yahoo.com>
2026-04-08 20:44:01 -0500

    update react and all

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,5 +1,5 @@
 // src/services/productService.js
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const fetchProductsAsync = async () => {
   const response = await fetch(`${API_BASE_URL}/products`);
